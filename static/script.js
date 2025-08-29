e24581ef89e7fd5ff98448e888b652a69ae49ef1
heidarosmalldoll80 <riyensarahh@hotmail.com>
2025-08-28 20:59:56 -0500

    feat: initial commit for static/script.js

---
+++ static/script.js
@@ -0,0 +1,7 @@
+document.getElementById('recordBtn').addEventListener('click', startRecording);
+
+function startRecording() {
+    // Sprachaufnahme und -verarbeitung
+    alert('Aufnahme gestartet (Demo)!');
+    // Hier würde die Logik zur Sprachaufnahme implementiert werden.
+}
